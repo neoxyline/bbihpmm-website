e594b94b8c7240c7ebc49b99656b62d6bca801a3
wiryorudolf <wiryodnc@gmail.com>
2024-11-07 10:41:48 +0800

    Update News.tsx guys

--- src/layouts/WebpageLayout.tsx
+++ src/layouts/WebpageLayout.tsx
@@ -1,5 +1,6 @@
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import News from "../components/News"
 
 type WebpageLayout = {
     page: any
@@ -12,6 +13,7 @@ export default function WebpageLayout({ page }: WebpageLayout) {
             <div className="px-4 md:px-28">
                 {page}
             </div>
+            <News />
             <Footer />
         </>
     )
