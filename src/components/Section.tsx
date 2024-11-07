07dfae8a19b1412cc54d25ffebfe43c42f9d019c
Akbar Angkasa <akbar121202@gmail.com>
2024-11-07 16:48:56 +0800

    fixes

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,8 +1,5 @@
-import React from 'react'
-
 export default function Section() {
     return (
-
         <div className="gap-4 items-center px-4 py-4 mx-auto max-w-6xl" id="accordion-open" data-accordion="open">
             <h2 id="accordion-open-heading-1">
                 <button type="button" className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-open-body-1" aria-expanded="true" aria-controls="accordion-open-body-1">
