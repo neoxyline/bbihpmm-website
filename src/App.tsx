d031d5edbe85183163a8b1d3756ba9b850a0be46
khairulgz <khairulgazali10@gmail.com>
2024-11-07 15:25:47 +0800

    menambahkan halaman tentang kami

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import WebpageLayout from './layouts/WebpageLayout';
-import RootPage from './app/index.tsx';
-import ProfilPage from './app/profil/index.tsx';
-import SejarahPage from './app/profil/sejarah/index.tsx';
-import VisiMisiPage from './app/profil/visi-misi/index.tsx';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WebpageLayout from "./layouts/WebpageLayout";
+import RootPage from "./app/index.tsx";
+import ProfilPage from "./app/profil/index.tsx";
+import SejarahPage from "./app/profil/sejarah/index.tsx";
+import VisiMisiPage from "./app/profil/visi-misi/index.tsx";
+import TentangBBIHPMM from "./app/profil/tentang-bbihpmm/index.tsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route 
-          path="/" 
-          element={
-            <WebpageLayout page={<RootPage />} />
-          } 
+        <Route path="/" element={<WebpageLayout page={<RootPage />} />} />
+        <Route
+          path="/profil"
+          element={<WebpageLayout page={<ProfilPage />} />}
         />
-        <Route 
-          path="/profil" 
-          element={
-            <WebpageLayout page={<ProfilPage />} />
-          } 
+        <Route
+          path="/profil/sejarah"
+          element={<WebpageLayout page={<SejarahPage />} />}
         />
-        <Route 
-          path="/profil/sejarah" 
-          element={
-            <WebpageLayout page={<SejarahPage />} />
-          } 
+        <Route
+          path="/profil/visi-misi"
+          element={<WebpageLayout page={<VisiMisiPage />} />}
         />
-        <Route 
-          path="/profil/visi-misi" 
-          element={
-            <WebpageLayout page={<VisiMisiPage />} />
-          } 
+        <Route
+          path="/profil/tentang-bbihpmm"
+          element={<WebpageLayout page={<TentangBBIHPMM />} />}
         />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
