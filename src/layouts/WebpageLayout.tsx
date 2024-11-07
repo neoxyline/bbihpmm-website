0e1397cbe01cbc25140d8543fc2515d103bee676
17dedeardiansyah <dhede.ardiansyah@gmail.com>
2024-11-07 14:07:38 +0800

    terbaru

--- src/layouts/WebpageLayout.tsx
+++ src/layouts/WebpageLayout.tsx
@@ -1,32 +1,11 @@
-<<<<<<< HEAD
-import Footer from "../components/Footer"
-import Keunggulan from "../components/Keunggulan"
-import Navbar from "../components/Navbar"
-import Section from "../components/Section"
-=======
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
->>>>>>> 5443766a8fd14838f67796cf5ddf4db36f878ba0
 
 type WebpageLayout = {
   page: any;
 };
 
 export default function WebpageLayout({ page }: WebpageLayout) {
-<<<<<<< HEAD
-    return (
-        <>
-            <Navbar />
-            <div className="px-4 md:px-28">
-                {page}
-            </div>
-            <Keunggulan />
-            <Section />
-            <Footer />
-        </>
-    )
-}
-=======
   return (
     <>
       <Navbar />
@@ -35,4 +14,3 @@ export default function WebpageLayout({ page }: WebpageLayout) {
     </>
   );
 }
->>>>>>> 5443766a8fd14838f67796cf5ddf4db36f878ba0
