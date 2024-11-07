2313af4b5e01e6a153c7cff8bf8ee8219bc80687
Akbar Angkasa <akbar121202@gmail.com>
2024-11-07 15:46:50 +0800

    modified Navbar.tsx, added UnhandlesPath.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,18 @@ import RootPage from './app/index.tsx';
 import ProfilPage from './app/profil/index.tsx';
 import SejarahPage from './app/profil/sejarah/index.tsx';
 import VisiMisiPage from './app/profil/visi-misi/index.tsx';
+import UnhandledPath from './components/UnhandledPath.tsx';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route 
+          path="*" 
+          element={
+            <WebpageLayout page={<UnhandledPath />} />
+          } 
+        />
         <Route 
           path="/" 
           element={
