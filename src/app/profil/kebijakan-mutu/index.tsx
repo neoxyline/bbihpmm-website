616e4f3843cd3e871d99d43cf615e6ee8dd64d24
17dedeardiansyah <dhede.ardiansyah@gmail.com>
2024-11-07 17:47:31 +0800

    Kebijakan

--- src/app/profil/kebijakan-mutu/index.tsx
+++ src/app/profil/kebijakan-mutu/index.tsx
@@ -5,9 +5,9 @@ export default function KebijakanMutuPage() {
     return (
         <>
             <div className="text-center text-4xl m-10 font-bold ">Kebijakan Mutu</div>
-            <div className="justify-center">
-                <h2 className="block max-w-2xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mb-2 text-lg text-gray-900 dark:text-white">Balai Besar Industri Hasil Perkebunan berpedoman pada peraturan perundang-undangan yang berlaku serta peningkatan mutu pelayanan secara terus menerus, siap melayani masyarakat dengan cara:</h2>
-                <ul className=" text-justify space-y-1 text-gray-500 list-inside dark:text-gray-400">
+            <div className="justify-center m-4 gap-4">
+                <div className="m-auto block max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mb-2 text-lg text-gray-900 dark:text-white">Balai Besar Industri Hasil Perkebunan berpedoman pada peraturan perundang-undangan yang berlaku serta peningkatan mutu pelayanan secara terus menerus, siap melayani masyarakat dengan cara:</div>
+                <ul className="m-auto block max-w-4xl p-4 text-left space-y-1 text-gray-500 list-inside dark:text-gray-400">
                     <Ceklis description="Meningkatkan kepuasan pelanggan/stakeholder melalui peningkatan mutu pelayanan jasa." />
                     <Ceklis description="Meningkatkan kualifikasi dan kompetensi pegawai untuk mendukung kegiatan pelayanan jasa teknis." />
                     <Ceklis description="Meningkatkan mutu manajemen lembaga melalui perbaikan tata cara kerja dan tata kelola organisasi ke arah efisiensi dan efektivitas organisasi." />
