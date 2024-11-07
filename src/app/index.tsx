be293ea37523abb2e6e408182f5da6be06d6c645
Akbar Angkasa <akbar121202@gmail.com>
2024-11-07 13:27:38 +0800

    fix / route, fix WebPageLayout.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
 import Jumbotron from "../components/Jumbotron";
+import Keunggulan from "../components/Keunggulan";
+import Layanan from "../components/Layanan";
 
 export default function RootPage(){
     return (
         <>
             <Jumbotron />
+            <Layanan />
+            <Keunggulan />
         </>
     )
 }
