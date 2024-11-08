efa3ff39e6bc53c725eda33b091580d0fb0331d6
wiryorudolf <wiryodnc@gmail.com>
2024-11-08 09:08:16 +0800

    profil pejabat

--- src/components/ProfilPejabat.tsx
+++ src/components/ProfilPejabat.tsx
@@ -1,38 +1,167 @@
+import kabagtu from '../assets/images/pejabat/agusgumiwang.png'
+
 export default function ProfilPejabat() {
-    return (
-        <div className="bg-slate-500 max-w-full w-full h-96 px-4">
-            <div className="bg-slate-50 shadow-md mx-auto px-7 py-2 w-[95%] h-[310px] flex justify-end">
-            <div className="bg-black rounded-md gap-3 w-[80%] h-[100%] mx-auto">
-                {/* <div className="w-[100%]">
-                <div className="max-w-md h-2/3 rounded-t-lg">
-                </div>
-                <div className="bg-white shadow-md max-w-md h-1/3 rounded-b-lg px-3 py-1 content-center">
-                </div>
-                </div>
-                <div className="w-[100%]">
-                <div className="max-w-md h-2/3 rounded-t-lg">
-                </div>
-                <div className="bg-white shadow-md max-w-md h-1/3 rounded-b-lg px-3 py-1 content-center">
-                </div>
-                </div> */}
+   return (
+      <div className="bg-slate-50 max-w-full px-4 mb-16">
+         <div className="px-7 py-5 h-72 flex flex-wrap gap-4 justify-center">
+
+            <div className="w-2/3 group relative overflow-hidden rounded-lg transform transition-transform duration-300 ease-out hover:-translate-y-2 hover:shadow-lg gap-4 flex flex-wrap justify-center p-4">
+               <div className="w-auto rounded-b-lg px-1 py-auto content-center">
+                  <h1 className="text-center text-black font-bold">Dr. Shinta Virdhian, S.T., MS.C</h1>
+                  <h1 className="text-center text-black font-light">Kepala BBSPJIHPMM</h1>
+               </div>
+               <div className="h-48 overflow-hidden flex justify-center">
+                  <img src={kabagtu} alt="" className="max-w-md rounded-t-lg h-48" />
+               </div>
+               {/* Hover effect */}
+               <div className="absolute inset-0 bg-slate-50 bg-opacity-95 flex flex-col justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-out p-9">
+                  <h1 className="text-blue-950 font-bold text-2xl leading-7">Dr. Shinta Virdhian, S.T., MS.C</h1>
+                  <p className="text-blue-600 font-semibold">Kepala BBSPJIHPMM</p>
+                  <p className="text-slate-500 font-normal  text-sm">Jl. Perintis Kemerdekaan No. 28 A Kota Makassar</p>
+                  <button className="bg-slate-200 px-4 py-1 text-lg text-blue-950 font-semibold rounded-full hover:text-white hover:bg-blue-950 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">Lihat Lebih Detail</button>
+               </div>
             </div>
+
+         </div>
+
+         <div className="bg-slate-50 mx-auto px-7 py-2 w-[95%] flex flex-wrap gap-7 justify-center">
+            <div className="w-96 group relative overflow-hidden rounded-lg transform transition-transform duration-300 ease-out hover:-translate-y-2 hover:shadow-lg">
+               <div className="w-auto h-48 overflow-hidden flex justify-center">
+                  <img src={kabagtu} alt="" className="max-w-md rounded-t-lg h-48" />
+               </div>
+               <div className="shadow-md w-auto rounded-b-lg px-1 py-auto content-center">
+                  <h1 className="text-center text-black font-bold">Abdul Rahman, S.Kom</h1>
+                  <h1 className="text-center text-black font-light">Kepala Bagian Tata Usaha</h1>
+               </div>
+               {/* Hover effect */}
+               <div className="absolute inset-0 bg-slate-50 bg-opacity-95 flex flex-col justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-out p-9">
+                  <h1 className="text-blue-950 font-bold text-2xl leading-7">Abdul Rahman, S.Kom</h1>
+                  <p className="text-blue-600 font-semibold">Kepala Bagian Tata Usaha</p>
+                  <p className="text-slate-500 font-normal  text-sm">Jl. Perintis Kemerdekaan No. 28 A Kota Makassar</p>
+                  <button className="bg-slate-200 px-4 py-1 text-lg text-blue-950 font-semibold rounded-full hover:text-white hover:bg-blue-950 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">Lihat Lebih Detail</button>
+               </div>
             </div>
-            <div className="bg-lime-300 shadow-md mx-auto px-7 py-2 w-[95%] h-[310px] flex justify-end">
-            <div className="bg-black rounded-md gap-3 w-[80%] h-[100%] mx-auto">
-                {/* <div className="w-[100%]">
-                <div className="max-w-md h-2/3 rounded-t-lg">
-                </div>
-                <div className="bg-white shadow-md max-w-md h-1/3 rounded-b-lg px-3 py-1 content-center">
-                </div>
-                </div>
-                <div className="w-[100%]">
-                <div className="max-w-md h-2/3 rounded-t-lg">
-                </div>
-                <div className="bg-white shadow-md max-w-md h-1/3 rounded-b-lg px-3 py-1 content-center">
-                </div>
-                </div> */}
+
+            <div className="w-96 group relative overflow-hidden rounded-lg transform transition-transform duration-300 ease-out hover:-translate-y-2 hover:shadow-lg">
+               <div className="w-auto h-48 overflow-hidden flex justify-center">
+                  <img src={kabagtu} alt="" className="max-w-md rounded-t-lg h-48" />
+               </div>
+               <div className="shadow-md w-auto rounded-b-lg px-1 py-auto content-center">
+                  <h1 className="text-center text-black font-bold">Ainun Berliani, S.Sos., M.I.Kom</h1>
+                  <h1 className="text-center text-black font-light">Ketua Tim Kemitraan</h1>
+               </div>
+               {/* Hover effect */}
+               <div className="absolute inset-0 bg-slate-50 bg-opacity-95 flex flex-col justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-out p-9">
+                  <h1 className="text-blue-950 font-bold text-2xl leading-7">Ainun Berliani, S.Sos., M.I.Kom</h1>
+                  <p className="text-blue-600 font-semibold">Ketua Tim Kemitraan</p>
+                  <p className="text-slate-500 font-normal  text-sm">Jl. Perintis Kemerdekaan No. 28 A Kota Makassar</p>
+                  <button className="bg-slate-200 px-4 py-1 text-lg text-blue-950 font-semibold rounded-full hover:text-white hover:bg-blue-950 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">Lihat Lebih Detail</button>
+               </div>
             </div>
+
+            <div className="w-96 group relative overflow-hidden rounded-lg transform transition-transform duration-300 ease-out hover:-translate-y-2 hover:shadow-lg">
+               <div className="w-auto h-48 overflow-hidden flex justify-center">
+                  <img src={kabagtu} alt="" className="max-w-md rounded-t-lg h-48" />
+               </div>
+               <div className="shadow-md w-auto rounded-b-lg px-1 py-auto content-center">
+                  <h1 className="text-center text-black font-bold">Drs. Muh. Idham, M.M</h1>
+                  <h1 className="text-center text-black font-light">Ketua Tim Promosi dan Kerjasama</h1>
+               </div>
+               {/* Hover effect */}
+               <div className="absolute inset-0 bg-slate-50 bg-opacity-95 flex flex-col justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-out p-9">
+                  <h1 className="text-blue-950 font-bold text-2xl leading-7">Drs. Muh. Idham, M.M</h1>
+                  <p className="text-blue-600 font-semibold">Ketua Tim Promosi dan Kerjasama</p>
+                  <p className="text-slate-500 font-normal  text-sm">Jl. Perintis Kemerdekaan No. 28 A Kota Makassar</p>
+                  <button className="bg-slate-200 px-4 py-1 text-lg text-blue-950 font-semibold rounded-full hover:text-white hover:bg-blue-950 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">Lihat Lebih Detail</button>
+               </div>
             </div>
-        </div>
-    )
+
+            <div className="w-96 group relative overflow-hidden rounded-lg transform transition-transform duration-300 ease-out hover:-translate-y-2 hover:shadow-lg">
+               <div className="w-auto h-48 overflow-hidden flex justify-center">
+                  <img src={kabagtu} alt="" className="max-w-md rounded-t-lg h-48" />
+               </div>
+               <div className="shadow-md w-auto rounded-b-lg px-1 py-auto content-center">
+                  <h1 className="text-center text-black font-bold">Erwin Adinata, M.Si</h1>
+                  <h1 className="text-center text-black font-light">Ketua Tim Data dan Informasi</h1>
+               </div>
+               {/* Hover effect */}
+               <div className="absolute inset-0 bg-slate-50 bg-opacity-95 flex flex-col justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-out p-9">
+                  <h1 className="text-blue-950 font-bold text-2xl leading-7">Erwin Adinata, M.Si</h1>
+                  <p className="text-blue-600 font-semibold">Ketua Tim Data dan Informasi</p>
+                  <p className="text-slate-500 font-normal  text-sm">Jl. Perintis Kemerdekaan No. 28 A Kota Makassar</p>
+                  <button className="bg-slate-200 px-4 py-1 text-lg text-blue-950 font-semibold rounded-full hover:text-white hover:bg-blue-950 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">Lihat Lebih Detail</button>
+               </div>
+            </div>
+
+            <div className="w-96 group relative overflow-hidden rounded-lg transform transition-transform duration-300 ease-out hover:-translate-y-2 hover:shadow-lg">
+               <div className="w-auto h-48 overflow-hidden flex justify-center">
+                  <img src={kabagtu} alt="" className="max-w-md rounded-t-lg h-48" />
+               </div>
+               <div className="shadow-md w-auto rounded-b-lg px-1 py-auto content-center">
+                  <h1 className="text-center text-black font-bold">Mamang,S.TP., M.Si</h1>
+                  <h1 className="text-center text-black font-light">Ketua Tim Pengujian, Kalibrasi, dan Verifikasi</h1>
+               </div>
+               {/* Hover effect */}
+               <div className="absolute inset-0 bg-slate-50 bg-opacity-95 flex flex-col justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-out p-9">
+                  <h1 className="text-blue-950 font-bold text-2xl leading-7">Mamang,S.TP., M.Si</h1>
+                  <p className="text-blue-600 font-semibold">Ketua Tim Pengujian, Kalibrasi, dan Verifikasi</p>
+                  <p className="text-slate-500 font-normal  text-sm">Jl. Perintis Kemerdekaan No. 28 A Kota Makassar</p>
+                  <button className="bg-slate-200 px-4 py-1 text-lg text-blue-950 font-semibold rounded-full hover:text-white hover:bg-blue-950 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">Lihat Lebih Detail</button>
+               </div>
+            </div>
+
+            <div className="w-96 group relative overflow-hidden rounded-lg transform transition-transform duration-300 ease-out hover:-translate-y-2 hover:shadow-lg">
+               <div className="w-auto h-48 overflow-hidden flex justify-center">
+                  <img src={kabagtu} alt="" className="max-w-md rounded-t-lg h-48" />
+               </div>
+               <div className="shadow-md w-auto rounded-b-lg px-1 py-auto content-center">
+                  <h1 className="text-center text-black font-bold">Hari Purwanto, M.Si</h1>
+                  <h1 className="text-center text-black font-light">Ketua Tim Sertifikasi</h1>
+               </div>
+               {/* Hover effect */}
+               <div className="absolute inset-0 bg-slate-50 bg-opacity-95 flex flex-col justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-out p-9">
+                  <h1 className="text-blue-950 font-bold text-2xl leading-7">Hari Purwanto, M.Si</h1>
+                  <p className="text-blue-600 font-semibold">Ketua Tim Sertifikasi</p>
+                  <p className="text-slate-500 font-normal  text-sm">Jl. Perintis Kemerdekaan No. 28 A Kota Makassar</p>
+                  <button className="bg-slate-200 px-4 py-1 text-lg text-blue-950 font-semibold rounded-full hover:text-white hover:bg-blue-950 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">Lihat Lebih Detail</button>
+               </div>
+            </div>
+
+            <div className="w-96 group relative overflow-hidden rounded-lg transform transition-transform duration-300 ease-out hover:-translate-y-2 hover:shadow-lg">
+               <div className="w-auto h-48 overflow-hidden flex justify-center">
+                  <img src={kabagtu} alt="" className="max-w-md rounded-t-lg h-48" />
+               </div>
+               <div className="shadow-md w-auto rounded-b-lg px-1 py-auto content-center">
+                  <h1 className="text-center text-black font-bold">Dr. Asma Assa, M.Si</h1>
+                  <h1 className="text-center text-black font-light">Ketua Tim Optimalisasi Pemanfaatan Teknologi Industri</h1>
+               </div>
+               {/* Hover effect */}
+               <div className="absolute inset-0 bg-slate-50 bg-opacity-95 flex flex-col justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-out p-9">
+                  <h1 className="text-blue-950 font-bold text-2xl leading-7">Dr. Asma Assa, M.Si</h1>
+                  <p className="text-blue-600 font-semibold">Ketua Tim Optimalisasi Pemanfaatan Teknologi Industri</p>
+                  <p className="text-slate-500 font-normal  text-sm">Jl. Perintis Kemerdekaan No. 28 A Kota Makassar</p>
+                  <button className="bg-slate-200 px-4 py-1 text-lg text-blue-950 font-semibold rounded-full hover:text-white hover:bg-blue-950 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">Lihat Lebih Detail</button>
+               </div>
+            </div>
+
+            <div className="w-96 group relative overflow-hidden rounded-lg transform transition-transform duration-300 ease-out hover:-translate-y-2 hover:shadow-lg">
+               <div className="w-auto h-48 overflow-hidden flex justify-center">
+                  <img src={kabagtu} alt="" className="max-w-md rounded-t-lg h-48" />
+               </div>
+               <div className="shadow-md w-auto rounded-b-lg px-1 py-auto content-center">
+                  <h1 className="text-center text-black font-bold">Yulismulianti, M.Si</h1>
+                  <h1 className="text-center text-black font-light">Ketua Tim Halal</h1>
+               </div>
+               {/* Hover effect */}
+               <div className="absolute inset-0 bg-slate-50 bg-opacity-95 flex flex-col justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-out p-9">
+                  <h1 className="text-blue-950 font-bold text-2xl leading-7">Yulismulianti, M.Si</h1>
+                  <p className="text-blue-600 font-semibold">Ketua Tim Halal</p>
+                  <p className="text-slate-500 font-normal  text-sm">Jl. Perintis Kemerdekaan No. 28 A Kota Makassar</p>
+                  <button className="bg-slate-200 px-4 py-1 text-lg text-blue-950 font-semibold rounded-full hover:text-white hover:bg-blue-950 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">Lihat Lebih Detail</button>
+               </div>
+            </div>
+
+         </div>
+      </div>
+   )
 }
